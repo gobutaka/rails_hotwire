72ff6b33072dc51e66d2cb5c4026eae0314b235e
gobutaka <takaaki.hirakura@game8.jp>
2022-06-05 12:25:09 +0900

    fixed validates

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,5 +1,5 @@
 class Pokemon < ApplicationRecord
   validates :name, presence: true
-  validates :pokedex_number, presence: true, numericality: {only_integer: true}
-  validates :total_base_stats, presence: true, numericality: {only_integer: true}
+  validates :pokedex_number, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :total_base_stats, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 end
